3aabf4fa46bb318eb8236704399dd7e367120423
SAMSAM <samprogrammation@gmail.com>
2025-05-14 00:08:15 -0400

    full rewrite of ItemCarrousel; bugfixing, readability and scalability improvments

--- Assets/Scripts/UI/Item_caroussel.cs
+++ Assets/Scripts/UI/Item_caroussel.cs
@@ -1,40 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class Item_caroussel : MonoBehaviour
 {
-    private LevelStatus levelStatus;
-    private Transform[] carrouselButtons;
     public GameObject carrousselButtonPrefab;
-
+    public float animationSpeed;
     public float scrollSensitivity;
 
-    private RectTransform carrouselTransform;
+    private LevelStatus levelStatus;
+    private RectTransform[] carrouselButtons;
+    private RectTransform carrousselRT;
 
-    public  float animationTime;
+    private Queue<Animation> animationQueue = new Queue<Animation>();
 
     private Camera canvasCamera;
+    public float boundPadding;
+    private float parentCanvasWidth;
+    private float buttonSize;
+    private float buttonScale;
+    private float carrouselWidth;
+    public float boundReajustSpeed;
     private float clickOffset;
 
-    private int carrouselButtonCount;
-    private float carrouselBoundLeft = 0f;
-    private float carrouselBoundRight = 0f;
-    public float halfSquare;
-    public float boundReajustTime;
+    private Vector2 carrouselVelocity = Vector2.zero;
+    private Vector2 scrollStartPosition = Vector2.zero;
 
-    private List<int> appearAnimationQueue = new List<int>();
-
-    private Vector3 emptyVector = Vector3.zero;
-    private Vector3 scrollStartPosition = Vector3.zero;
     private bool isScrolling;
     private bool previouslyScrolling;
     private bool isAnimating;
 
+    struct Animation
+    {
+        public enum AnimationType { APPEARING, DISAPPEARING };
+        public AnimationType type;
+        public int index;
+
+        public Animation(AnimationType type, int index)
+        {
+            this.type = type;
+            this.index = index;
+        }
+    }
+
     void Start()
     {
+        buttonScale = carrousselButtonPrefab.GetComponent<RectTransform>().localScale.x;
+        buttonSize = carrousselButtonPrefab.GetComponent<RectTransform>().sizeDelta.x * buttonScale;
+
+        carrousselRT = GetComponent<RectTransform>();
+        carrousselRT.anchoredPosition = new Vector2(boundPadding, carrousselRT.anchoredPosition.y);
+
         Camera[] cameras = new Camera[Camera.allCamerasCount];
         Camera.GetAllCameras(cameras); 
         foreach (Camera cam in cameras)
@@ -47,64 +66,73 @@ public class Item_caroussel : MonoBehaviour
         }
 
         levelStatus = Camera.main.GetComponent<LevelStatus>();
-        carrouselTransform = transform.GetComponent<RectTransform>();
-        carrouselButtonCount = levelStatus.ItemCount();
-        carrouselButtons = new Transform[levelStatus.ItemCount()];
+        carrouselButtons = new RectTransform[levelStatus.ItemCount()];
+
+        parentCanvasWidth = GetComponentInParent<Canvas>().GetComponent<RectTransform>().rect.width;
+        carrouselWidth = buttonSize * levelStatus.ItemCount() * transform.localScale.x;
 
         for (int i = 0; i < levelStatus.ItemCount(); i++)
         {
-            carrouselButtons[i] = Instantiate(carrousselButtonPrefab).GetComponent<Transform>();
+            carrouselButtons[i] = Instantiate(carrousselButtonPrefab).GetComponent<RectTransform>();
             carrouselButtons[i].SetParent(transform);
             carrouselButtons[i].GetComponent<CarrouselButton>().Initialize(levelStatus.GetItem(i));
-            carrouselButtons[i].localPosition = new Vector3(-446 + 2*halfSquare*i, 0, 0);
-            carrouselButtons[i].localScale = Vector3.one;
+            carrouselButtons[i].anchoredPosition = new Vector2(buttonSize * i, 0);
+            carrouselButtons[i].localScale = buttonScale * Vector2.one;
         }
 
-        if(levelStatus.ItemCount() > 5f)
-        {
-            float initialBoundOffset = (levelStatus.ItemCount() - 5f) * 180f;
-            carrouselBoundLeft = -initialBoundOffset;
-            carrouselBoundRight = initialBoundOffset;
-        }
+        GetComponent<BoxCollider>().size = new Vector3(carrouselWidth + 2 * boundPadding, carrousselRT.sizeDelta.y, 1);
+        GetComponent<BoxCollider>().center = new Vector3(carrouselWidth / 2 - boundPadding, 0, 0);
+
     }
-    
+
     void Update()
     {
         bool touchExists = Input.touchCount > 0;
         bool clickExists = Input.GetMouseButton(0);
         if (touchExists || clickExists)
-            castCarrouselRay(canvasCamera.ScreenPointToRay(touchExists ? Input.GetTouch(0).position : Input.mousePosition), touchExists);
+            CastCarrouselRay(canvasCamera.ScreenPointToRay(touchExists ? Input.GetTouch(0).position : Input.mousePosition), touchExists);
         else
             isScrolling = false;
 
         if (isScrolling) return;
 
-        if (transform.localPosition.x > carrouselBoundRight)
-            transform.localPosition = Vector3.SmoothDamp(transform.localPosition, new Vector3(carrouselBoundRight, transform.localPosition.y, transform.localPosition.z), ref emptyVector, boundReajustTime);
-        else if (transform.localPosition.x < carrouselBoundLeft)
-            transform.localPosition = Vector3.SmoothDamp(transform.localPosition, new Vector3(carrouselBoundLeft, transform.localPosition.y, transform.localPosition.z), ref emptyVector, boundReajustTime);
+        if (carrousselRT.anchoredPosition.x + Mathf.Max(carrouselWidth, parentCanvasWidth - 2 * boundPadding) < parentCanvasWidth - boundPadding)
+        {
+            Vector2 objective = new Vector3(parentCanvasWidth - boundPadding - Mathf.Max(carrouselWidth, parentCanvasWidth - 2 * boundPadding), carrousselRT.anchoredPosition.y);
+            carrousselRT.anchoredPosition = Vector2.SmoothDamp(carrousselRT.anchoredPosition, objective, ref carrouselVelocity, 1 / boundReajustSpeed);
+        }
+        else if (carrousselRT.anchoredPosition.x > boundPadding)
+        {
+            Vector2 objective = new Vector3(boundPadding, carrousselRT.anchoredPosition.y);
+            carrousselRT.anchoredPosition = Vector2.SmoothDamp(carrousselRT.anchoredPosition, objective, ref carrouselVelocity, 1 / boundReajustSpeed);
+        }
+
+        if (!isAnimating && animationQueue.Count != 0) 
+        {
+            StartCoroutine(CarrouselAnimation(animationQueue.Dequeue()));
+        }
     }
 
-    private void castCarrouselRay(Ray ray, bool touchExists)
+    private void CastCarrouselRay(Ray ray, bool touchExists)
     {
         RaycastHit hitInfo;
         if (!Physics.Raycast(ray, out hitInfo, 10f)) { isScrolling = false; return; }
 
         GameObject hitObject = hitInfo.collider.gameObject;
-        Vector3 currentPosition = ray.GetPoint(10f);
+        Vector2 currentPosition = ray.GetPoint(10f);
         if (!hitObject.CompareTag("Carrousel")) return;
 
         if (Input.GetMouseButtonDown(0) || (touchExists && Input.GetTouch(0).phase == TouchPhase.Began))
         {
             scrollStartPosition = currentPosition;
-            clickOffset = transform.position.x - currentPosition.x;
+            clickOffset = carrousselRT.position.x - currentPosition.x;
         }
         else
         {
             isScrolling |= (scrollStartPosition - currentPosition).magnitude > 1 / scrollSensitivity;
         }
 
-        transform.position = new Vector3(currentPosition.x + clickOffset, transform.position.y, transform.position.z);
+        carrousselRT.position = new Vector2(currentPosition.x + clickOffset, carrousselRT.position.y);
     }
 
 
@@ -113,18 +141,25 @@ public class Item_caroussel : MonoBehaviour
         previouslyScrolling = isScrolling;
     }
 
+    public void DisappearThenSlide(int carrouselButtonIndex)
+    {
+        animationQueue.Enqueue(new Animation(Animation.AnimationType.DISAPPEARING, carrouselButtonIndex));
+    }
+
+    public void AppearThenSlide(int carrouselButtonIndex)
+    {
+        animationQueue.Enqueue(new Animation(Animation.AnimationType.APPEARING, carrouselButtonIndex));
+    }
+
     public void DisappearThenSlide(Transform levelItem)
     {
         for (int i = 0; i < levelStatus.ItemCount(); i++)
         {
-            Transform carrouselButton = carrouselButtons[i];
-            if (carrouselButton.GetComponent<CarrouselButton>().LevelItem() == levelItem)
+            if (carrouselButtons[i].GetComponent<CarrouselButton>().LevelItem() == levelItem)
             {
-                StartCoroutine(disappearAnimation(carrouselButton));
-                StartCoroutine(slideIntoPlace(i, true));
+                DisappearThenSlide(i);
                 return;
             }
-
         }
     }
 
@@ -132,147 +167,59 @@ public class Item_caroussel : MonoBehaviour
     {
         for (int i = 0; i < levelStatus.ItemCount(); i++)
         {
-            Transform carrouselButton = carrouselButtons[i];
-            if (carrouselButton.GetComponent<CarrouselButton>().LevelItem() == levelItem)
+            if (carrouselButtons[i].GetComponent<CarrouselButton>().LevelItem() == levelItem)
             {
-
-                if (appearAnimationQueue.Count == 0)
-                {
-                    StartCoroutine(appearAnimation(carrouselButton));
-                    StartCoroutine(slideIntoPlace(i, false));
-                }
-
-                appearAnimationQueue.Add(i);
+                AppearThenSlide(i);
                 return;
             }
-
         }
     }
 
-    IEnumerator disappearAnimation(Transform carrouselButton)
+    IEnumerator CarrouselAnimation(Animation anim)
     {
+        bool isAppearing = anim.type == Animation.AnimationType.APPEARING;
+        int animationSign = isAppearing ? 1 : -1;
+
         isAnimating = true;
-        while (carrouselButton.localScale.x > 0.01f)
+        float animationProgress = 0;
+        float animationVelocity = 0;
+
+        Vector2[] initialPositions = new Vector2[carrouselButtons.Length];
+        for (int i = 0; i < carrouselButtons.Length; i++)
+            initialPositions[i] = carrouselButtons[i].anchoredPosition;
+
+
+        RectTransform animatedButton = carrouselButtons[anim.index];
+        if (isAppearing)
+            animatedButton.gameObject.SetActive(true);
+
+        while (animationProgress < 0.99f)
         {
-            float lerpUpdate = Mathf.Lerp(carrouselButton.localScale.x, 0, animationTime);
-            carrouselButton.localScale = new Vector3(lerpUpdate, lerpUpdate, 1f);
+            float slideScaling = Mathf.Lerp(0, buttonSize / 2, animationProgress);
+            float buttonScaling = isAppearing ? animationProgress : 1 - animationProgress;
+            animatedButton.localScale = buttonScale * (new Vector3(buttonScaling, buttonScaling, 1));
+            for (int i = anim.index + 1; i < carrouselButtons.Length; i++)
+            {
+                RectTransform curr = carrouselButtons[i];
+                Vector2 currInitPos = initialPositions[i];
+                curr.anchoredPosition = new Vector2(currInitPos.x + animationSign * 2 * slideScaling, currInitPos.y);
+            }
+            animationProgress = Mathf.SmoothDamp(animationProgress, 1f, ref animationVelocity, 1 / animationSpeed);
             yield return null;
         }
 
-        carrouselButton.gameObject.SetActive(false);
-        carrouselButton.localScale = new Vector3(0f,0f,1f);
-
-        if(carrouselButtonCount > 5)
+        animatedButton.localScale = isAppearing? buttonScale * Vector3.one : Vector3.zero;
+        for (int i = anim.index + 1; i < carrouselButtons.Length; i++)
         {
-            carrouselBoundLeft += halfSquare;
-            carrouselBoundRight -= halfSquare;
+            RectTransform curr = carrouselButtons[i];
+            Vector2 currInitPos = initialPositions[i];
+            curr.anchoredPosition = new Vector2(currInitPos.x + animationSign * buttonSize, currInitPos.y);
         }
-        carrouselButtonCount -= 1;
+
+        if (!isAppearing)
+            animatedButton.gameObject.SetActive(false);
+        carrouselWidth += animationSign * buttonSize;
         isAnimating = false;
-    }
-
-    IEnumerator appearAnimation(Transform carrouselButton)
-    {
-        isAnimating = true;
-        carrouselButton.gameObject.SetActive(true);
-
-        while (carrouselButton.localScale.x <= 0.99f)
-        {
-            float lerpUpdate = Mathf.Lerp(carrouselButton.localScale.x, 1, animationTime);
-            carrouselButton.localScale = new Vector3(lerpUpdate, lerpUpdate, animationTime);
-            yield return null;
-        }
-
-        carrouselButton.localScale = Vector3.one;
-        carrouselButtonCount += 1;
-
-        if (carrouselButtonCount > 5)
-        {
-            carrouselBoundLeft -= halfSquare;
-            carrouselBoundRight += halfSquare;
-        }
-    }
-
-    IEnumerator slideIntoPlace(int clickedButtonIndex, bool isDisappearing)
-    {
-        bool slidingComplete = false;
-        List<float> targetPosition = new List<float>();
-        if (isDisappearing)
-        {
-            for (int i=0; i < carrouselButtons.Length; i++)
-            {
-                if (i < clickedButtonIndex)
-                {
-
-                    targetPosition.Add(carrouselButtons[i].localPosition.x + halfSquare);
-                }
-                else if ( i > clickedButtonIndex)
-                {
-                    targetPosition.Add(carrouselButtons[i].localPosition.x - halfSquare);
-                }
-                else
-                {
-                    targetPosition.Add(carrouselButtons[i].localPosition.x);
-                }
-            }
-        }
-        else
-        {
-            for (int i = 0; i < carrouselButtons.Length; i++)
-            {
-                if (i < clickedButtonIndex)
-                {
-                    targetPosition.Add(carrouselButtons[i].localPosition.x - halfSquare);
-                }
-                else if (i > clickedButtonIndex)
-                {
-                    targetPosition.Add(carrouselButtons[i].localPosition.x + halfSquare);
-                }
-                else if (i == clickedButtonIndex)
-                {
-                    targetPosition.Add(carrouselButtons[i].localPosition.x);
-                }
-            }
-        }
-
-        
-
-        while (!slidingComplete)
-        {
-            slidingComplete = true;
-            
-            for (int i = 0; i < carrouselButtons.Length; i++)
-            {
-                carrouselButtons[i].localPosition = new Vector3(Mathf.Lerp(carrouselButtons[i].localPosition.x, targetPosition[i], animationTime), carrouselButtons[i].localPosition.y, carrouselButtons[i].localPosition.z);
-                
-                if (!(carrouselButtons[i].localPosition.x <= targetPosition[i] + 1f && carrouselButtons[i].localPosition.x >= targetPosition[i] - 1f))
-                {
-                    slidingComplete = false;
-                }
-            }
-            
-            yield return null;
-        }
-
-        for (int i=0; i < carrouselButtons.Length; i++)
-        {
-            carrouselButtons[i].localPosition = new Vector3(targetPosition[i], carrouselButtons[i].localPosition.y, carrouselButtons[i].localPosition.z);
-        }
-
-        if (appearAnimationQueue.Count <= 1)
-        {
-            isAnimating = false;
-        }
-        else
-        {
-            StartCoroutine(appearAnimation(carrouselButtons[appearAnimationQueue[1]]));
-            StartCoroutine(slideIntoPlace(appearAnimationQueue[1], false));
-        }
-
-        if(!isDisappearing)
-        {
-            appearAnimationQueue.RemoveAt(0);
-        }
     }
 
     public bool IsScrolling()
